781f8cd2638a5ff64841e0eafa94c2cf9592890a
EJ Raday <ernestjraday@gmail.com>
2026-03-25 14:31:20 -0400

    Update sketch.js

--- dev/sketch.js
+++ dev/sketch.js
@@ -1,7 +1,23 @@
+var playerX = 25
+var playerY = 300
+
+function setupPlayfield() {
+  background(220)
+  rect(0, 350, 400, 50)
+  rect(playerX, playerY, 50, 50)
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  setupPlayfield()
+  if (keyIsDown(65)) {
+    playerX -= 5;
+  }
+  if (keyIsDown(68)) {
+    playerX += 5;
+  }
+
 }
