e3fe7e6621e4b365f1e2f5adf54f5911022e27f9
Alexander <delac2ac@dukes.jmu.edu>
2026-03-27 11:40:24 -0400

    Add title screen functionality and fullscreen canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let gameState = "title";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
 }
 
-function draw() {
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() { // change to wanted game state.
+  if (gameState === "title") {
+    drawTitleScreen();
+  } else {
+    return;
+  }
+}
+
+function drawGamePrototype() {
   background(220);
+  fill(20);
+  noStroke();
+  textSize(24);
+  text("Game Prototype Running", width / 2, height / 2);
+}
+
+function keyPressed() {
+  if (gameState === "title") {
+    gameState = handleTitleKeyPressed();
+  }
 }
