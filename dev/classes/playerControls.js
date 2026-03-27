422170b35c0f9eb81bff83b8af16350a81f0a809
EJ Raday <ernestjraday@gmail.com>
2026-03-27 11:01:35 -0400

    Classes

---
+++ dev/classes/playerControls.js
@@ -0,0 +1,9 @@
+// export function keyListener() {
+//     setupPlayfield()
+//     if (keyIsDown(65)) {
+//         playerX -= 5;
+//     }
+//     if (keyIsDown(68)) {
+//         playerX += 5;
+//     }
+// }
