0b5bc238f7e947c0d2ae2413d444a1c7c4fdd196
EJ Raday <ernestjraday@gmail.com>
2026-03-29 20:22:22 -0400

    more jumping configuration

--- dev/classes/playerControls.js
+++ dev/classes/playerControls.js
@@ -3,6 +3,9 @@ const gravity = 0.5
 let yVelocity = 0;
 
 function jumpDebounceTimeout() {
+    //TODO: TEST THIS
+    //TODO: LANDED CHECK
+    //TODO: FIGURE OUT HOW TO DETECT LANDING
     setTimeout(() => {
         jumpDebounce = false;
         print("db reset")
@@ -11,12 +14,15 @@ function jumpDebounceTimeout() {
 
 function keyListener() {
     if (keyIsDown(65)) {
+        //  A
         playerX -= 5;
     }
     if (keyIsDown(68)) {
+        //  D
         playerX += 5;
     }
-    if (keyIsDown(32) && !jumpDebounce) {
+    if ((keyIsDown(32) || keyIsDown(87)) && !jumpDebounce) {
+        //  W or Space
         yVelocity = -10;
         jumpDebounce = true
         jumpDebounceTimeout();
