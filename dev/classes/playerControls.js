f09e86726985c70ab75420f3e54461909c2d8f04
EJ Raday <ernestjraday@gmail.com>
2026-03-30 10:55:46 -0400

    root to dev

--- dev/classes/playerControls.js
+++ dev/classes/playerControls.js
@@ -1,33 +1,12 @@
-var jumpDebounce = false
-const gravity = 0.5
-let yVelocity = 0;
-
-function jumpDebounceTimeout() {
-    //TODO: TEST THIS
-    //TODO: LANDED CHECK
-    //TODO: FIGURE OUT HOW TO DETECT LANDING
-    setTimeout(() => {
-        jumpDebounce = false;
-        print("db reset")
-    }, 500);
-}
-
-function keyListener() {
-    if (keyIsDown(65)) {
-        //  A
-        playerX -= 5;
-    }
-    if (keyIsDown(68)) {
-        //  D
-        playerX += 5;
-    }
-    if ((keyIsDown(32) || keyIsDown(87)) && !jumpDebounce) {
-        //  W or Space
-        yVelocity = -10;
-        jumpDebounce = true
-        jumpDebounceTimeout();
-    }
-
-    yVelocity += gravity;
-    playerY += yVelocity;
+function applyPlayerControls(player) {
+  if (keyIsDown(65)) {
+    player.x -= player.moveSpeed;
+  }
+  if (keyIsDown(68)) {
+    player.x += player.moveSpeed;
+  }
+  if ((keyIsDown(32) || keyIsDown(87)) && player.isOnGround) {
+    player.yVelocity = -player.jumpStrength;
+    player.isOnGround = false;
+  }
 }
