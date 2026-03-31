d7590270d83150c4ebf35d3b0df2536ad34641fe
EJ Raday <ernestjraday@gmail.com>
2026-03-31 18:14:17 -0400

    Fixed platform table, added floor, added player implementation to dev sketch

--- dev/classes/Level.js
+++ dev/classes/Level.js
@@ -3,11 +3,21 @@ class Level {
         //table of platforms, not drawn yet
         this.platforms = platforms;
         this.background = backgroundImage;
+        //floor, do not include in level platforms
+        var floor = new Platform(width / 2, height, width, 50);
+        platforms.push(floor);
         image(this.background, 0, 0, width, height);
     }
     drawPlatforms() {
         for (let platform of this.platforms) {
             platform.draw();
         }
+
+        
     }
+    drawLevel() {
+        image(this.background, 0, 0, width, height);
+        this.drawPlatforms();
+    }
+    
 }
--- dev/sketch.js
+++ dev/sketch.js
@@ -1,48 +1,54 @@
 let gameState = "title";
 
-level1Platforms = {
-  platform1: new Platform(200, 400, 300, 20),
-  platform2: new Platform(500, 300, 200, 20),
-  platform3: new Platform(800, 500, 250, 20),
-  platform4: new Platform(1100, 350, 150, 20),
-  platform5: new Platform(1400, 450, 300, 20)
-}
+let platforms;
+let player;
+
+level1Platforms = [
+  new Platform(200, 400, 300, 20),
+  new Platform(500, 300, 200, 20),
+  new Platform(800, 500, 250, 20),
+  new Platform(1100, 350, 150, 20),
+  new Platform(1400, 450, 300, 20)
+]
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
-
   rectMode(CENTER);
-  //platform = new Platform(200, 400, 300, 20);
+  setupLevel();
+}
+
+function setupLevel() {
+  //platform = new Platform(width / 2, height * 0.72, 320, 20);
+  level = new Level(level1Platforms);
+  level.drawPlatforms();
+  player = new Player(width * .2, height-100, 40, 60);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  setupLevel();
 }
 
 function draw() { // change to wanted game state.
   if (gameState === "title") {
     drawTitleScreen();
   } else if (gameState === "playing") {
-    background(220);
-    level1 = new Level(level1Platforms);
-    level1.drawPlatforms()
-    //platform.draw();
+    level.drawLevel();
+    player.update(level1Platforms);
+    player.draw();
   }
 }
 
 function drawGamePrototype() {
-  background(220);
   fill(20);
   noStroke();
   textSize(24);
   text("Game Prototype Running", width / 2, height / 2);
 }
 
-
-
 function keyPressed() {
   if (gameState === "title") {
-    handleTitleKeyPressed()
+    handleTitleKeyPressed();
   }
 }
