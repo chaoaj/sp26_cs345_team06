57d456007ef0bca883b1f7a0142cd758eb7bc62e
tironae <tironale@dukes.jmu.edu>
2026-04-02 14:16:45 -0400

    fixed laser, adding another feature later

--- dev/classes/LaserTrap.js
+++ dev/classes/LaserTrap.js
@@ -1,18 +1,15 @@
 class LaserTrap extends Trap {
-  constructor(x, y, w, h, damage = 1, fireDuration = 2000) {
+  constructor(x, y, w, h, damage = 1, fireDuration = 2000, repeatInterval = 5000) {
     super(x, y, w, h, damage);
     this.fireDuration = fireDuration;
+    this.repeatInterval = max(repeatInterval, fireDuration);
   }
 
   isFiring() {
-    return millis() < this.fireDuration;
+    return millis() % this.repeatInterval < this.fireDuration;
   }
 
   draw() {
-    if (!this.isFiring()) {
-      return;
-    }
-
     const left = this.x - this.w / 2;
     const top = this.y - this.h / 2;
 
@@ -20,6 +17,11 @@ class LaserTrap extends Trap {
     noStroke();
     fill(70);
     rect(left - 18, top - 6, 18, this.h + 12, 6);
+
+    if (!this.isFiring()) {
+      rectMode(CENTER);
+      return;
+    }
 
     stroke(255, 40, 40);
     strokeWeight(this.h);
