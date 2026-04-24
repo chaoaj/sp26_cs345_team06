467ed1d5fc43f9e7fc46da69b4bba4ce6f970a38
mohouail <mikotokim@aucegypt.edu>
2026-04-23 20:33:43 -0400

    Reorganized assets folder

--- dev/classes/Preload.js
+++ dev/classes/Preload.js
@@ -3,75 +3,75 @@ function preload() {
   // so preload here
 
   //Music
-  backgroundMusic = loadSound("assets/BackgroundMusic.mp3")
-  soliloquyMusic = loadSound("assets/Soliloquy.mp3")
+  backgroundMusic = loadSound("assets/audio/BackgroundMusic.mp3")
+  soliloquyMusic = loadSound("assets/audio/Soliloquy.mp3")
 
   //Sound Effects
 
   //Items
-  featherImage = loadImage("assets/Feather.png")
-  healthImage = loadImage("assets/heart.png")
-  speedImage = loadImage("assets/SpeedPotion.png")
-  shieldImage = loadImage("assets/Shield.png")
-  
+  featherImage = loadImage("assets/abilities/collectibles/Feather.png")
+  healthImage = loadImage("assets/abilities/collectibles/heart.png")
+  speedImage = loadImage("assets/abilities/collectibles/SpeedPotion.png")
+  shieldImage = loadImage("assets/abilities/collectibles/Shield.png")
+
   //Ability UI
-  doublejumpui = loadImage("assets/doublejumpui.png")
-  dashui = loadImage("assets/dashui.png")
+  doublejumpui = loadImage("assets/abilities/UI_icons/doublejumpui.png")
+  dashui = loadImage("assets/abilities/UI_icons/dashui.png")
 
   //Ability Items
-  doubleJumpAmuletImage = loadImage("assets/DoubleJumpAmulet.png")
-  dashAmuletImage = loadImage("assets/DashAmulet.png")
+  doubleJumpAmuletImage = loadImage("assets/abilities/collectibles/DoubleJumpAmulet.png")
+  dashAmuletImage = loadImage("assets/abilities/collectibles/DashAmulet.png")
 
   //Terrain
-  step1 = loadImage("assets/step1.png")
-  step2 = loadImage("assets/step2.png")
-  step3 = loadImage("assets/step3.png")
-  step4 = loadImage("assets/step4.png")
-  box1 = loadImage("assets/box1.png")
-  box2 = loadImage("assets/box2.png")
-  box3 = loadImage("assets/box3.png")
-  box4 = loadImage("assets/box4.png")
-  box4long = loadImage("assets/box4-long.png")
+  step1 = loadImage("assets/terrain/step1.png")
+  step2 = loadImage("assets/terrain/step2.png")
+  step3 = loadImage("assets/terrain/step3.png")
+  step4 = loadImage("assets/terrain/step4.png")
+  box1 = loadImage("assets/objects/box1.png")
+  box2 = loadImage("assets/objects/box2.png")
+  box3 = loadImage("assets/objects/box3.png")
+  box4 = loadImage("assets/objects/box4.png")
+  box4long = loadImage("assets/objects/box4-long.png")
 
   //Title Screen
-  titleScreenimage = loadImage("assets/Title.png")
-  startInstructionImage = loadImage("assets/Start.png")
-  layer1 = loadImage("assets/Layer1.png")
-  layer2 = loadImage("assets/Layer2.png")
-  layer3 = loadImage("assets/Layer3.png")
-  layer4 = loadImage("assets/Layer4.png")
-  layer5 = loadImage("assets/Layer5.png")
-  attribution = loadImage("assets/attribution.png")
+  titleScreenimage = loadImage("assets/title_screen/Title.png")
+  startInstructionImage = loadImage("assets/title_screen/Start.png")
+  layer1 = loadImage("assets/title_screen/Layer1.png")
+  layer2 = loadImage("assets/title_screen/Layer2.png")
+  layer3 = loadImage("assets/title_screen/Layer3.png")
+  layer4 = loadImage("assets/title_screen/Layer4.png")
+  layer5 = loadImage("assets/title_screen/Layer5.png")
+  attribution = loadImage("assets/title_screen/attribution.png")
 
   //Backgrounds
-  backgroundImage = loadImage("assets/Background1.png");
+  backgroundImage = loadImage("assets/title_screen/Background1.png");
 
   //Floors
-  brickFloorImage = loadImage("assets/Floor1.png");
-  floorTileLevel1 = loadImage("assets/FloorTileLvl1.png")
-  floorTileLevel2 = loadImage("assets/FloorTileLvl2.png")
-  floorTileLevel3 = loadImage("assets/FloorTileLvl3.png")
-  lavaImage = loadImage("assets/Lava.png")
+  brickFloorImage = loadImage("assets/tiles/Floor1.png");
+  floorTileLevel1 = loadImage("assets/tiles/FloorTileLvl1.png")
+  floorTileLevel2 = loadImage("assets/tiles/FloorTileLvl2.png")
+  floorTileLevel3 = loadImage("assets/tiles/FloorTileLvl3.png")
+  lavaImage = loadImage("assets/tiles/Lava.png")
 
   //Platforms
-  brickPlatformImage = loadImage("assets/BrickPlatform.png")
-  brickTileImage = loadImage("assets/bricktile.png")
+  brickPlatformImage = loadImage("assets/tiles/BrickPlatform.png")
+  brickTileImage = loadImage("assets/tiles/bricktile.png")
 
   //Object Sprites
-  woodenBox = loadImage("assets/WoodenBox.png")
-  spikeTrap = loadImage("assets/SpikeTrap.png")
-  doorImage = loadImage("assets/door.png")
+  woodenBox = loadImage("assets/objects/WoodenBox.png")
+  spikeTrap = loadImage("assets/objects/SpikeTrap.png")
+  doorImage = loadImage("assets/objects/door.png")
 
   //Player Sprites
-  playerIdleSheet = loadImage("assets/chacter_sprite/1/Idle.png");
-  playerWalkSheet = loadImage("assets/chacter_sprite/1/Walk.png");
-  playerRunSheet  = loadImage("assets/chacter_sprite/1/Run.png");
-  playerJumpSheet = loadImage("assets/chacter_sprite/1/Jump.png");
-  playerHurtSheet = loadImage("assets/chacter_sprite/1/Hurt.png");
+  playerIdleSheet = loadImage("assets/characters/player/1/Idle.png");
+  playerWalkSheet = loadImage("assets/characters/player/1/Walk.png");
+  playerRunSheet  = loadImage("assets/characters/player/1/Run.png");
+  playerJumpSheet = loadImage("assets/characters/player/1/Jump.png");
+  playerHurtSheet = loadImage("assets/characters/player/1/Hurt.png");
 
   //Enemy Sprites
-  greenslimeimage = loadImage("assets/SlimeWalk.png")
+  greenslimeimage = loadImage("assets/enemies/SlimeWalk.png")
 
   //UI
-  heartImage = loadImage("assets/heart.png")
+  heartImage = loadImage("assets/abilities/collectibles/heart.png")
 }
